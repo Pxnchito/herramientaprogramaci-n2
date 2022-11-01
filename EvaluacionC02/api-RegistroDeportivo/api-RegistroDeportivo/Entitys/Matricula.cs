adaf17a112b4b82a72e24546a9e7d22b75e69887
Pxnchito <navarrofrancisco232@gmail.com>
2022-11-01 02:41:15 -0500

    Evaluacion 02. 01/11/22
    Evaluación Continua 02

---
+++ EvaluacionC02/api-RegistroDeportivo/api-RegistroDeportivo/Entitys/Matricula.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_RegistroDeportivo.Entitys
+{
+    public class Matricula
+    {
+        [Key]
+        public int codigo { get; set;}
+        
+        [Required]
+        [StringLength(
+                maximumLength: 50,
+                ErrorMessage = "Se tiene que ingresar un nombre")]
+        public string direccion { get; set; }
+        [Required]
+        public string telefono { get; set; }
+
+        [Required]
+        public int dni { get; set; }
+        
+        public Alumno alumno { get; set; }
+    }
+}
---
+++ EvaluacionC02/api-RegistroDeportivo/api-RegistroDeportivo/Migrations/20221101054821_Inicial.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api_RegistroDeportivo.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alumno",
+                columns: table => new
+                {
+                    dni = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    departamento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    distrito = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    edad = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alumno", x => x.dni);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matricula",
+                columns: table => new
+                {
+                    codigo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    direccion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    dni = table.Column<int>(type: "int", nullable: false),
+                    alumnodni = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matricula", x => x.codigo);
+                    table.ForeignKey(
+                        name: "FK_Matricula_Alumno_alumnodni",
+                        column: x => x.alumnodni,
+                        principalTable: "Alumno",
+                        principalColumn: "dni",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matricula_alumnodni",
+                table: "Matricula",
+                column: "alumnodni");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Matricula");
+
+            migrationBuilder.DropTable(
+                name: "Alumno");
+        }
+    }
+}
